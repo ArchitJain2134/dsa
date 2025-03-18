3c2931b430be3e723504e191c8870713a0d4137e
ArchitJain2134 <144250786+ArchitJain2134@users.noreply.github.com>
2025-03-18 17:22:59 +0530

    fixed an error

--- minAndMaxVal/main.go
+++ minAndMaxVal/main.go
@@ -15,6 +15,7 @@ func main() {
 	}
 
 	// printing an array which the user is give as input to confirm
+	    fmt.Println("Array given by you: ")
 		fmt.Println(arr)
 		
 	// calculating teh value by calling the function
